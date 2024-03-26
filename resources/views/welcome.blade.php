4f97fea25032462fd23d7f795c84932a80be20da
Md. Iqbal Khan <iqbalkhan319@gmail.com>
2024-03-26 23:12:23 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Welcome to the forum</h1>
+                    <h1>Welcome to Jenkins pipeline</h1>
                 </div>
 
             </div>
